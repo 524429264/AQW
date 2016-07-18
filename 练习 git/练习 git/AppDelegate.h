c2e00302c5037bb2e15f284f218d619e35e8a9f6
零距离仰望星空 <524429264@qq.com>
2016-07-18 12:49:45 +0800

    创建新工程
    新工程

---
+++ 练习 git/练习 git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  练习 git
+//
+//  Created by tyh on 16/7/18.
+//  Copyright © 2016年 tyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 练习 git/练习 git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  练习 git
+//
+//  Created by tyh on 16/7/18.
+//  Copyright © 2016年 tyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
